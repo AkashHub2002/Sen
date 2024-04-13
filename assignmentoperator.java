a36bc4b46df6e906d8ba05690324bb4a0bdc0802
Somnath Biswas <147364879+SomnathB2002@users.noreply.github.com>
2024-04-14 00:14:23 +0530

    Add files via upload

---
+++ assignmentoperator.java
@@ -0,0 +1,10 @@
+public class assignmentoperator {
+    public static void main(String[] args) {
+        int a =5;
+        // a+=10;
+        // a-=10;
+        // a*=10;
+        a/=10;
+        System.out.println(a);
+    }
+}
---
+++ binaryoperator.java
@@ -0,0 +1,12 @@
+// Arithmetic Binary Opeator
+public class binaryoperator {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("add = " + (A+B));
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
